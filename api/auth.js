c9a6831d48609a1c0d6b51709ff2b1aacb565818
guyKremer <42372467+guyKremer@users.noreply.github.com>
2019-09-06 15:17:15 +0300

    finonefix + user field fix

--- api/auth.js
+++ api/auth.js
@@ -14,16 +14,20 @@ function userAuthentication(req,res,next){
 
 //needs encription
 function emailPasswordValidation(req,res,next){
-    const user = User.findOne({email:req.body.email,password:req.body.password});
-    if(user!==null){
-        req.userid = user._id;
-        next();
+    const user = await User.findOne({email:req.body.email,password:req.body.password}).exec()
+    .then((user) =>{
+        if(user!==null){
+            req.userid = user._id;
+            next();
+        }
+        else{
+            res.status(401).json({
+                message:"User Unuthenticated"
+            })
+        }
     }
-    else{
-        res.status(401).json({
-            message:"User Unuthenticated"
-        })
-    }
+);
+
 
 }
 module.exports = {userAuthentication,emailPasswordValidation};

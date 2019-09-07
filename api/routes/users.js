ff8c081eae9795db13052c7fa06d2105d5208eb3
guyKremer <42372467+guyKremer@users.noreply.github.com>
2019-09-07 13:12:30 +0300

    session deletion in users

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,26 +6,9 @@ const router = express.Router();
 const User = require ('../../models/user');
 const Auth = require ('../auth');
 // insert new User
-router.post('/',(req, res, next) => {
-	const user = new User(req.body);
-	req.session._id= user._id;
-	try{
-		user.save()
-		.then(result => {
-			res.status(201).json({
-				message: "Handle Post req to /users",
-				createdUser: result
-			})
-		});
-	}
-	catch(err){
-		console.log(err.message);
-	}
-	
-});
 
-router.get('/',(req,res,next)=>{
-	User.findOne({_id: req.session._id})
+router.get('/:id',(req,res,next)=>{
+	User.findOne({_id: req.params.id})
 	.then(response=>{
 			res.status(200).json(response);
 		})

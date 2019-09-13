78d16deba0d6072dedca543f4571b11364810bfb
naorbakal <37515568+naorbak@users.noreply.github.com>
2019-09-13 13:12:16 +0300

    update: bad req if client email null

--- api/routes/nutritionists.js
+++ api/routes/nutritionists.js
@@ -11,9 +11,8 @@ const meetingUtils = require('./commons/meetingsUtils');
 
 router.post('/:nutritionistID/users',async (req,res,next)=>{
 	const nutritionist = await User.findById(req.params.nutritionistID).exec();	
-	console.log(req.body);
+
 	if(req.body.clientEmail){
-		console.log("in if");
 	User.findOne({email: req.body.clientEmail})
 	.then(client=>{
 		console.log(client);
@@ -54,8 +53,7 @@ router.post('/:nutritionistID/users',async (req,res,next)=>{
 	});	
 }
 	else { 
-		console.log("in else");
-		res.status(200).json(nutritionist.clientsIDs);		
+		res.status(400).json({message : "problem with Client Email"});		
 	}
 });
 
